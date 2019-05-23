8eefa4fdc9306afa4f9b94e5e7b5eeb3e1cb64b5
Lukie Kang <lukiek+f@gmail.com>
2019-05-23 22:04:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(collection)
-
+ new_collection = []
   i = 0
   while i < collection.size
-    yield(collection[i])
+    new_collection << yield(collection[i])
     i += 1
   end
-
+  new_collection
 end
